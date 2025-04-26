79c72d3a8cde8ddd4cb651ed5363e05c705ee299
Lucas <lucas@Lucass-MacBook-Pro.local>
2025-04-26 13:57:24 -0400

    aerospace.toml, nvim/init.lua, and .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Maximize on startup
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
+-- Font
+config.font_size = 20
+
+config.color_scheme = "Adventure Time"
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+
+-- and finally, return the configuration to wezterm
+return config
